ebeee4a9f7b05955d039e2569e8bd86b837915ec
Turan Ketene <turank97@gmail.com>
2019-12-15 00:08:15 -0800

    Updates

---
+++ precache-manifest.ddb6b50d291b9a5815b046ad78172a99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa61727ca2744df60aebc0f1f299d87",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "91ad7e498ded8973aa8b",
+    "url": "/Clicky-Game/static/css/main.b0301a6a.chunk.css"
+  },
+  {
+    "revision": "c43780d072d862424898",
+    "url": "/Clicky-Game/static/js/2.10ea4d50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.10ea4d50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91ad7e498ded8973aa8b",
+    "url": "/Clicky-Game/static/js/main.45b80dc0.chunk.js"
+  },
+  {
+    "revision": "26ee0aba715578d6f77c",
+    "url": "/Clicky-Game/static/js/runtime-main.5d0af5bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
